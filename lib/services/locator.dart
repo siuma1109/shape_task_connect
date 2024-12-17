87adea662b707edf800f9adc8a49f816763b5b47
siuma1109 <matsunkit1109@gmail.com>
2024-12-18 02:30:45 +0800

    Add Biometrics feature for android

--- lib/services/locator.dart
+++ lib/services/locator.dart
@@ -2,11 +2,21 @@ import 'package:get_it/get_it.dart';
 import 'auth_service.dart';
 import 'database_service.dart';
 import '../repositories/user_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final locator = GetIt.instance;
 
-void setupLocator() {
+Future<void> setupLocator() async {
+  final prefs = await SharedPreferences.getInstance();
+
+  // Register SharedPreferences first
+  locator.registerSingleton<SharedPreferences>(prefs);
+
+  // Register services in order of dependency
   locator.registerLazySingleton(() => DatabaseService());
-  locator.registerLazySingleton(() => UserRepository(locator()));
-  locator.registerLazySingleton(() => AuthService(locator()));
+  locator
+      .registerLazySingleton(() => UserRepository(locator<DatabaseService>()));
+  locator.registerLazySingleton(
+    () => AuthService(locator<UserRepository>(), locator<SharedPreferences>()),
+  );
 }
