cc62069c86c9d224c0184219bd44d136033d4029
siuma1109 <matsunkit1109@gmail.com>
2024-12-18 03:06:09 +0800

    add back id for user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,9 +1,11 @@
 class User {
+  final int? id;
   final String email;
   final String username;
   final String password;
 
   User({
+    this.id,
     required this.email,
     required this.username,
     required this.password,
@@ -11,6 +13,7 @@ class User {
 
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'email': email,
       'username': username,
       'password': password,
@@ -19,6 +22,7 @@ class User {
 
   factory User.fromMap(Map<String, dynamic> map) {
     return User(
+      id: map['id'],
       email: map['email'],
       username: map['username'],
       password: map['password'],
