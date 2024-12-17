fa0af335175e5a979c1503b545ca42f5956a4363
siuma1109 <matsunkit1109@gmail.com>
2024-12-18 03:02:10 +0800

    Fix conflict

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,11 +1,9 @@
 class User {
-  final int? id;
   final String email;
   final String username;
   final String password;
 
   User({
-    this.id,
     required this.email,
     required this.username,
     required this.password,
@@ -13,7 +11,6 @@ class User {
 
   Map<String, dynamic> toMap() {
     return {
-      'id': id,
       'email': email,
       'username': username,
       'password': password,
@@ -22,7 +19,6 @@ class User {
 
   factory User.fromMap(Map<String, dynamic> map) {
     return User(
-      id: map['id'],
       email: map['email'],
       username: map['username'],
       password: map['password'],
